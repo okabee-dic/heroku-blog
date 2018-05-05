771f1d423dc248a187be4aff7d4e74af7b2edafa
Hiraku Okabe <okabee326@yahoo.co.jp>
2018-05-05 09:04:42 +0000

    commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,43 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+  def new
+    @blog = Blog.new
+  end
+  
+  def create
+    @blog = Blog.new(blog_param)
+    
+    if @blog.save
+      redirect_to blog_path
+    else
+      render "new"
+    end
+  end
+  
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+  
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    
+    redirect_to blogs_path, notice: "ブログを削除しました！"
+  end
+  
+  private
+  def blog_param
+     params.require(:blog).permit(:title, :content) 
+  end
+end
